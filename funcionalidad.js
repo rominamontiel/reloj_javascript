185ab9a84293a00ba1aa669cd783116f988051ff
rominamontiel <romiiemma@hotmail.com>
2019-12-07 20:41:50 -0300

    agrego la fecha, es funcional

--- funcionalidad.js
+++ funcionalidad.js
@@ -5,12 +5,24 @@ const seg = document.querySelector("#seg");
 
 setInterval(() => {
     let day = new Date();
+
     let hh = day.getHours() * 30;
     let mm = day.getMinutes() * deg;
     let ss = day.getSeconds() * deg;
 
     hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
     min.style.transform = `rotateZ(${mm}deg)`;
-    seg.style.transform = `rotateZ(${ss}deg)`;
-
+    seg.style.transform = `rotateZ(${ss}deg)`;    
 });
+
+let day = new Date();
+
+var dia = day.getDate();
+var mes = day.getMonth() + 1;
+var año = day.getFullYear();
+
+var fecha = document.createElement('h1');
+fecha.innerHTML = dia + ' . ' + mes + ' . ' + año;
+document.getElementById('leyenda').appendChild(fecha);
+
+

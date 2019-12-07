63071517508fc32605dc65e595a04b42e9aac99b
rominamontiel <romiiemma@hotmail.com>
2019-12-07 17:46:39 -0300

    primer commit, subiendo las cosas

---
+++ funcionalidad.js
@@ -0,0 +1,16 @@
+const deg = 6;
+const hr = document.querySelector("#hr");
+const min = document.querySelector("#min");
+const seg = document.querySelector("#seg");
+
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    min.style.transform = `rotateZ(${mm}deg)`;
+    seg.style.transform = `rotateZ(${ss}deg)`;
+
+});
